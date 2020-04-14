600ed0c484af297970409ee99b53b674b8abfde8
Chase Shriver <chase802@icloud.com>
2020-04-14 17:08:16 -0400

    added bad admin page

--- routes/index.js
+++ routes/index.js
@@ -14,22 +14,32 @@ router.get('/aboutus', (req, res) => {
 });
 
 router.get('/getinvolved', (req, res) => {
-  console.log('Request for contact page recieved');
+  console.log('Request for involved page recieved');
   res.render('getinvolved');
 });
 
 router.get('/ourmission', (req, res) => {
-    console.log('Request for contact page recieved');
+    console.log('Request for mission page recieved');
     res.render('ourmission');
   });
 
   router.get('/ourpeople', (req, res) => {
-    console.log('Request for contact page recieved');
+    console.log('Request for people page recieved');
     var mongoUtil = require( './mongoUtil' );
     var db = mongoUtil.getDb();
     db.collection('kids').find().toArray((err, result) => {
       if (err) return console.log(err)
       res.render('ourpeople', {database: result})
+    })
+  });
+
+  router.get('/admin', (req, res) => {
+    console.log('Request for admin page recieved');
+    var mongoUtil = require( './mongoUtil' );
+    var db = mongoUtil.getDb();
+    db.collection('kids').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      res.render('admin', {database: result})
     })
   });
 
